fd9124b934fe52646f654ef3131c3e4f93ed56ae
Nasaee <nasaee.dev@gmail.com>
2025-09-21 02:47:03 +0700

    implement first query

--- project_sql/1_top_paying_jobs.sql
+++ project_sql/1_top_paying_jobs.sql
@@ -6,6 +6,25 @@ Question: What are the top-paying data analyst jobs?
 - Why? Highlight the top-paying opportunities for Data Analysts, offering insights into employment options and location flexibility.
 */
 
+SELECT 
+job_id,
+name AS company_name,
+job_title,
+job_location,
+job_schedule_type,
+salary_year_avg,
+job_posted_date
+FROM 
+	job_postings_fact AS jp
+LEFT JOIN company_dim AS cd ON jp.company_id = cd.company_id
+WHERE
+	job_title_short = 'Data Analyst' AND 
+	job_location = 'Anywhere' AND
+	salary_year_avg IS NOT NULL
+ORDER BY
+	salary_year_avg DESC
+LIMIT 10;
+
 /*
 Here's the breakdown of the top data analyst jobs in 2023:
 Wide Salary Range: Top 10 paying data analyst roles span from $184,000 to $650,000, indicating significant salary potential in the field.
